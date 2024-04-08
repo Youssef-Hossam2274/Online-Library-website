288bc605ada315cde56c089d97fdfe12ce655bf6
Youssef-Hossam2274 <y.hossam2274@gmail.com>
2024-04-08 22:10:04 +0200

    separate  header and footer in files

---
+++ JS/footer.js
@@ -0,0 +1,12 @@
+let footer = 
+            `
+            <footer>
+                <ul class="footer-links">
+                    <li><a href="#">About us</a></li>
+                    <li><a href="#">Contact us</a></li>
+                    <li><a href="#">Terms of use</a></li>
+                    <li><a href="#">Privacy policy</a></li>
+                </ul>
+            </footer>
+            `
+document.write(footer);
---
+++ JS/header.js
@@ -0,0 +1,29 @@
+let header = 
+            `
+            <header class="website-header">
+                <div class="logo">
+                    <a href="Home.html"
+                        ><img src="../img/ICON.png" alt="ICON eror path" width="70"
+                    /></a>
+                    <h1>OnlineLibrary</h1>
+                </div>
+
+                <nav class="nav-links">
+                    <ul>
+                        <li><a href="Home.html">Home</a></li>
+                        <li><a href="../HTML/all_books.html">Books</a></li>
+                        <li>
+                            <a href="../HTML/borrowed_books.html">borrowed books (User)</a>
+                        </li>
+                        <li>
+                            <a href="../HTML/admin_book.html">list of added books (Admin)</a>
+                        </li>
+                        <li><a href="../HTML/profile.html">Profile</a></li>
+                        <li><a href="../HTML/Login.html">Login</a></li>
+                        <li><a href="../HTML/SignUp.html">Sign Up</a></li>
+                    </ul>
+                </nav>
+
+            </header>
+            `
+document.write(header);
