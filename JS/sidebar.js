70914ae9eb72cd833d327cb60b3009e0d56c35e8
Eslam-Sayed7 <eslam.sayed.ed@gmail.com>
2024-04-18 10:58:12 +0200

    sidebar clickable v4

---
+++ JS/sidebar.js
@@ -0,0 +1,19 @@
+function AccountDetailsClick() {
+    window.location.href = "AccountDetails.html";
+  }
+
+  function OrdersClick() {
+    window.location.href = "orders.html";
+  }
+
+  function AddressesClick() {
+    window.location.href = "addresses.html";
+  }
+
+  function PaymentClick() {
+    window.location.href = "paymentDetails.html";
+  }
+
+  function SignOutClick() {
+    window.location.href = "login.html";
+  }
