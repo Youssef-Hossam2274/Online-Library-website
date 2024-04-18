c72477c8442e371ecb9b27900c45d4aae883f094
Abdullah1Alnami <bdallhsydalnmy@gmail.com>
2024-04-18 20:05:23 +0200

    add_select

--- JS/products.js
+++ JS/products.js
@@ -1,11 +1,10 @@
-
-
 function searchBooks() {
+
     var input, filter, sections, i, j, books, book, title, txtValue;
 
     input = document.getElementById('searchInput');
     filter = input.value.toUpperCase();
-    sections = document.querySelectorAll("section.Book_sections");
+    sections = document.querySelectorAll("section[id^='']");
 
     for (i = 0; i < sections.length; i++) {
 
@@ -13,7 +12,7 @@ function searchBooks() {
         for (j = 0; j < books.length; j++) {
 
             book = books[j];
-            title = book.getElementsByTagName("h3")[0];
+            title = book.querySelector("h3")[0];
             txtValue = title.innerText;
 
             if (txtValue.toUpperCase().indexOf(filter) > -1) { book.style.display = ""; }
@@ -21,5 +20,22 @@ function searchBooks() {
             else { book.style.display = "none"; }
 
         }
+    }
+}
+
+function filterByCategory() {
+
+    var select, sections, i, category;
+
+    select = document.getElementById('categorySelect');
+    category = select.value.toLowerCase();
+    sections = document.querySelectorAll('section[id^=""]');
+
+    for (i = 0; i < sections.length; i++) {
+
+        if (sections[i].id.toLowerCase() !== category && category !== 'all') { sections[i].style.display = "none"; }
+
+        else { sections[i].style.display = "block"; }
+
     }
 }
