27d7fe2e0139cac2fa03a021c4af52f606f01408
Sayed Reda <sydr38734@gmail.com>
2024-04-19 02:11:22 +0200

    Added media queries to single book page

--- JS/zoom.js
+++ JS/zoom.js
@@ -1,33 +1,36 @@
 let bookImage = document.querySelector("#book-image");
 let zoomContainer = document.querySelector(".zoom-container");
-let zoomedImage = document.querySelector("#zoomed-image")
-// Adding the event to book image
-bookImage.addEventListener("mousemove", (e) => {
-    // Showing the container
-    zoomContainer.style.display = "block";
-    
-    // Extracting the current width and height
-    let width = parseFloat(window.getComputedStyle(bookImage).width);
-    let height = parseFloat(window.getComputedStyle(bookImage).height);
-    
-    // Extracting the current mouse position portion
-    let x = e.offsetX / width;
-    let y = e.offsetY / height;
+let zoomedImage = document.querySelector("#zoomed-image");
 
-    // Preserving x & y in the container
-    if (x >= 0.95) {
-        x = 0.95;
-    }
-    if (y >= 0.935) {
-        y = 0.935;
-    }
+// Adding the event to book image in-case of wide screen
+if (screen.width > 768) {
+    bookImage.addEventListener("mousemove", (e) => {
+        // Showing the container
+        zoomContainer.style.display = "block";
 
-    // Zooming the current part
-    zoomedImage.style.transform = `translate(-${x*100*0.6}%, -${y*100*0.8}%)`;
+        // Extracting the current width and height
+        let width = parseFloat(window.getComputedStyle(bookImage).width);
+        let height = parseFloat(window.getComputedStyle(bookImage).height);
 
-    console.log(x, y);
-});
+        // Extracting the current mouse position portion
+        let x = e.offsetX / width;
+        let y = e.offsetY / height;
 
-bookImage.addEventListener("mouseleave", (e) => {
-    zoomContainer.style.display = "none";
-});
+        // Preserving x & y in the container
+        if (x >= 0.95) {
+            x = 0.95;
+        }
+        if (y >= 0.935) {
+            y = 0.935;
+        }
+
+        // Zooming the current part
+        zoomedImage.style.transform = `translate(-${x * 100 * 0.6}%, -${
+            y * 100 * 0.8
+        }%)`;
+    });
+
+    bookImage.addEventListener("mouseleave", (e) => {
+        zoomContainer.style.display = "none";
+    });
+}
