e4ed20120685f093b41ca05e787db59ce47d86ff
Youssef-Hossam2274 <y.hossam2274@gmail.com>
2024-04-21 01:33:25 +0200

    Add user schema in local storge

--- JS/signUp.js
+++ JS/signUp.js
@@ -1,34 +1,93 @@
-let eyeIcon1 = document.getElementById("eyeIcon1");
-let eyeIcon2 = document.getElementById("eyeIcon2");
+let open_eye = document.getElementById("open-eye");
+let close_eye = document.getElementById("close-eye");
+let password = document.getElementById("password-input");
 
-let password = document.getElementById("password");
-let confirmPassword = document.getElementById("ConfirmPassword");
-
-eyeIcon1.onclick = function()
+open_eye.onclick = close_eye.onclick = function()
 {
     if(password.type == "password")
     {
-        password.type = "text";
-        eyeIcon1.src = "../img/open-eye.png";
+        password.type = "text"
+        open_eye.style.display = "flex";
+        close_eye.style.display = "none";
+        
     }
     else
     {
         password.type = "password";
-        eyeIcon1.src = "../img/close-eye.png";
+        open_eye.style.display = "none";
+        close_eye.style.display = "flex";
     }
 }
-eyeIcon2.onclick = function()
-{
-    if(confirmPassword.type == "password")
-    {
-        confirmPassword.type = "text";
-        eyeIcon2.src = "../img/open-eye.png";
-    }
-    else
-    {
-        confirmPassword.type = "password";
-        eyeIcon2.src = "../img/close-eye.png";
+
+let user = document.getElementById("user");
+let admin = document.getElementById("admin");
+
+user.onclick = function(){
+    user.classList.add("type-active");
+    admin.classList.remove("type-active");
+}
+
+admin.onclick = function(){
+    admin.classList.add("type-active");
+    user.classList.remove("type-active");
+}
+
+
+class User {
+    constructor(
+        userName,
+        password,
+        email,
+        isAdmin,               
+        firstName,
+        lastName,
+        imageURL,
+        books,
+        favorites
+    ) {
+        this.userName = userName;
+        this.password = password;
+        this.email = email;
+        this.isAdmin = isAdmin;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.imageURL = imageURL;
+        this.books = books;
+        this.favorites = favorites;
     }
 }
 
 
+
+function addNewUser(){
+    let userType = false;
+    if (admin.className === "admin type-active")
+        userType = true;
+
+    const newUser = new User(
+        document.getElementById("user-input").value,
+        document.getElementById("password-input").value,
+        document.getElementById("email-input").value,
+        userType,
+        "",
+        "",
+        "",
+        [],
+        []
+    );
+    let usersJSON = window.localStorage.getItem("users");
+    let updatedJSON, usersArr;
+
+    if (usersJSON) {
+        usersArr = JSON.parse(usersJSON);
+        usersArr.push(newUser);
+        updatedJSON = JSON.stringify(usersArr);
+    } else {
+        updatedJSON = JSON.stringify([newUser]);
+    }
+    window.localStorage.setItem("users", updatedJSON);
+}
+
+
+const myForm = document.querySelector(".signup-content");
+myForm.addEventListener("submit", addNewUser);
