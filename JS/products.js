a80c3c13efb5a7287171f7559d17ce40ef6f5def
Sayed Reda <sydr38734@gmail.com>
2024-04-21 21:06:58 +0200

    Fixed Add all books in case of book is null

--- JS/products.js
+++ JS/products.js
@@ -90,7 +90,8 @@ function set_categorty_select_list() {
 
 function AddAllBooks() {
     let books = JSON.parse(window.localStorage.getItem("books"));
-
+    if (!books) return;
+    
     for (let i = 0; i < books.length; i += 1) {
             if (books[i]) {
                 let currentBook = books[i];
