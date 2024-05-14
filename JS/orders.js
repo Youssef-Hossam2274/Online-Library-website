857928d6cd1bf9ad092518c7435e18665ba517fe
Youssef-Hossam2274 <y.hossam2274@gmail.com>
2024-05-14 23:22:23 +0300

    edit the borrow date in "Your Books"

--- JS/orders.js
+++ JS/orders.js
@@ -1,58 +1,128 @@
+// function AddAllBooks() {
+    
+//   let request =  new XMLHttpRequest();
+//   request.open("GET", "http://127.0.0.1:8000/api.BorrowTransaction/");
+//   request.send();
+//   request.onload = () =>{
+//       let data = JSON.parse(request.responseText);
+//       let booksIDs = [];
+//       for(const T of  data){
+//           if(T["user"] == userId)
+//               booksIDs.push(T["book"]);
+//       }
+      
+//       for(const bookID of booksIDs){
+//           let bookRequest = new XMLHttpRequest();
+//           bookRequest.open("GET", `http://127.0.0.1:8000/api.books/${bookID}`);
+//           bookRequest.send();
+//           bookRequest.onload = () =>{
+//               console.log(booksIDs);
+//               let bookData = JSON.parse(bookRequest.responseText);
+//               console.log(bookData);
+//               let order = `
+//                   <div class="order">
+//                     <div class="order-id">#${bookID}</div>
+//                     <div class="info">
+//                       <div class="title">${bookData["title"]}</div>
+//                       <div class="info-divider">
+//                         <hr class="order-divider">
+//                       </div>
+//                       <div class="order-date-container">
+//                         <div id="order-date">
+//                           <i class="fa-solid fa-calendar-days"></i> ${bookData["publish_date"]}
+//                         </div>
+//                         <div id="order-address">
+//                           <i class="fa-solid fa-location-dot"></i> 132 Main St
+//                         </div>
+//                         <div id="in-use-order-status">In-use</div>
+//                       </div>
+//                       <div class="des">
+//                         ${bookData["description"]}
+//                       </div>
+//                       <div id="toggle-buttons">
+//                         <button class="expand-button">Open Book</button>
+//                         <button class="cancel-button">Cancel Order</button>
+//                       </div>
+//                     </div>
+//                     <div class="cover-and-price">
+//                       <div id="book-cover">
+//                         <img src="../backend${bookData["cover"]}" />
+//                       </div>
+//                     </div>
+//                 </div>
+//               `;
+//               const parser = new DOMParser();
+//               const parsedDocument = parser.parseFromString(order, "text/html");
+              
+//               let MyMain = document.querySelector(".order-list");
+//               MyMain.append(parsedDocument.querySelector(".order"));
+//               document.querySelector("#book-cover img").style.width = "150px";
+//           }
+//       }
+//   }
+// }
+
+
 function AddAllBooks() {
-  let request = new XMLHttpRequest();
+    
+  let request =  new XMLHttpRequest();
   request.open("GET", "http://127.0.0.1:8000/api.BorrowTransaction/");
   request.send();
   request.onload = () =>{
-    
-  }
+      let data = JSON.parse(request.responseText);
+      let booksIDs = [];
+      for(const T of  data){
+          if(T["user"] == userId){
+            let bookRequest = new XMLHttpRequest();
+            bookRequest.open("GET", `http://127.0.0.1:8000/api.books/${T["book"]}`);
+            bookRequest.send();
+            bookRequest.onload = () =>{
+                console.log(booksIDs);
+                let bookData = JSON.parse(bookRequest.responseText);
+                console.log(bookData);
+                let order = `
+                    <div class="order">
+                      <div class="order-id">#${T["book"]}</div>
+                      <div class="info">
+                        <div class="title">${bookData["title"]}</div>
+                        <div class="info-divider">
+                          <hr class="order-divider">
+                        </div>
+                        <div class="order-date-container">
+                          <div id="order-date">
+                            <i class="fa-solid fa-calendar-days"></i> ${T["borrow_date"]}
+                          </div>
+                          <div id="order-address">
+                            <i class="fa-solid fa-location-dot"></i> 132 Main St
+                          </div>
+                          <div id="in-use-order-status">In-use</div>
+                        </div>
+                        <div class="des">
+                          ${bookData["description"]}
+                        </div>
+                        <div id="toggle-buttons">
+                          <button class="expand-button">Open Book</button>
+                          <button class="cancel-button">Cancel Order</button>
+                        </div>
+                      </div>
+                      <div class="cover-and-price">
+                        <div id="book-cover">
+                          <img src="../backend${bookData["cover"]}" />
+                        </div>
+                      </div>
+                  </div>
+                `;
+                const parser = new DOMParser();
+                const parsedDocument = parser.parseFromString(order, "text/html");
+                
+                let MyMain = document.querySelector(".order-list");
+                MyMain.append(parsedDocument.querySelector(".order"));
+                document.querySelector("#book-cover img").style.width = "150px";
+            }
 
-
-  for (let i = 0; i < booksID.length; i += 1) {
-      let curBookID = booksID[i];
-      if (books[curBookID]) {
-          let currentBook = books[curBookID];
-          console.log(currentBook);
-          let order = `
-          <div class="order">
-            <div class="order-id">#${curBookID}</div>
-            <div class="info">
-              <div class="title">${currentBook.title}</div>
-              <div class="info-divider">
-                <hr class="order-divider">
-              </div>
-              <div class="order-date-container">
-                <div id="order-date">
-                  <i class="fa-solid fa-calendar-days"></i> ${currentBook.publishDate}
-                </div>
-                <div id="order-address">
-                  <i class="fa-solid fa-location-dot"></i> 132 Main St
-                </div>
-                <div id="in-use-order-status">In-use</div>
-              </div>
-              <div class="des">
-                ${currentBook.description}
-              </div>
-              <div id="toggle-buttons">
-                <button class="expand-button">Open Book</button>
-                <button class="cancel-button">Cancel Order</button>
-              </div>
-            </div>
-            <div class="cover-and-price">
-              <div id="book-cover">
-                <img src=${currentBook.imageURL} />
-              </div>
-            </div>
-        </div>
-          `;
-
-          const parser = new DOMParser();
-          const parsedDocument = parser.parseFromString(order, "text/html");
-          
-          let MyMain = document.querySelector(".order-list");
-          MyMain.append(parsedDocument.querySelector(".order"));
-          document.querySelector("#book-cover img").style.width = "150px";
+          }
       }
   }
 }
 
-// AddAllBooks();
+AddAllBooks();
