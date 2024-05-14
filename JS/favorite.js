a43e2f90d79eca0732fa7ee8c917c6b14b85d56d
Youssef-Hossam2274 <y.hossam2274@gmail.com>
2024-05-14 22:45:56 +0300

    load favorite book in dynamic way

--- JS/favorite.js
+++ JS/favorite.js
@@ -10,37 +10,43 @@ function AddAllBooks() {
             if(T["user"] == userId)
                 booksIDs.push(T["book"]);
         }
-        // console.log(booksIDs);
-
+        
         for(const bookID of booksIDs){
             let bookRequest = new XMLHttpRequest();
             bookRequest.open("GET", `http://127.0.0.1:8000/api.books/${bookID}`);
             bookRequest.send();
             bookRequest.onload = () =>{
+                console.log(booksIDs);
+                let authorRequest = new XMLHttpRequest();
                 let bookData = JSON.parse(bookRequest.responseText);
-                let book =
-                `
-                <div class="Book">
-                    <div class="background-img">
-                        <a href="../HTML/book.html?id=${bookID}">
-                            <img src="../backend${bookData["cover"]}" />
-                        </a>
+                authorRequest.open("GET", `http://127.0.0.1:8000/api.authors/${bookData["author"]}/`);
+                authorRequest.send();
+                authorRequest.onload = () =>{
+                    let book =
+                    `
+                    <div class="Book">
+                        <div class="background-img">
+                            <a href="../HTML/book.html?id=${bookID}">
+                                <img src="../backend${bookData["cover"]}" />
+                            </a>
+                        </div>
+                        <div class="content-book">
+                            <h3>${bookData["title"]}</h3> 
+                            <span><strong>Author(s):</strong>${JSON.parse(authorRequest.responseText)["name"]}</span>
+                            <a href="../HTML/book.html?id=${bookID}">
+                                <button id= "ShowDetails">Show details</button> 
+                            </a>
+                        </div>
                     </div>
-                    <div class="content-book">
-                        <h3>${bookData["title"]}</h3> 
-                        <span><strong>Author(s):</strong>${bookData["author"]}</span>
-                        <a href="../HTML/book.html?id=${bookID}">
-                            <button id= "ShowDetails">Show details</button> 
-                        </a>
-                    </div>
-                </div>
-                `;
+                    `;
+    
+                    const parser = new DOMParser();
+                    const parsedDocument = parser.parseFromString(book, "text/html");
+    
+                    let MyMain = document.querySelector(".main-books");
+                    MyMain.append(parsedDocument.querySelector(".book"));
+                }
 
-                const parser = new DOMParser();
-                const parsedDocument = parser.parseFromString(book, "text/html");
-
-                let MyMain = document.querySelector(".main-books");
-                MyMain.append(parsedDocument.querySelector(".book"));
             }
         }
     }
--- JS/orders.js
+++ JS/orders.js
@@ -1,55 +1,11 @@
-let order = `
-    <div class="order">
-            <div class="order-id">#12545</div>
-            <div class="info">
-              <div class="title">Advanced Calculus</div>
-              <div class="info-divider">
-                <hr class="order-divider">
-              </div>
-              <div class="order-date-container">
-                <div id="order-date">
-                  <i class="fa-solid fa-calendar-days"></i> March 5, 2022
-                </div>
-                <div id="order-address">
-                  <i class="fa-solid fa-location-dot"></i> 132 Main St
-                </div>
-                <div id="in-use-order-status">In-use</div>
-              </div>
-              <div class="des">
-                Classic text leads from elementary calculus into more theoretic
-                problems. Precise approach with definitions, theorems, proofs,
-                examples and exercises. Topics include partial differentiation,
-                vectors, differential geometry, infinite series, gamma function,
-                Fourier series, Laplace transform, much more. Numerous graded
-                exercises with selected answers.
-              </div>
-              <div id="toggle-buttons">
-                <button class="expand-button">Open Book</button>
-                <button class="cancel-button">Cancel Order</button>
-              </div>
-            </div>
-            <div class="cover-and-price">
-              <div id="book-cover">
-                <img src="../img/Calculus.webp" />
-              </div>
-              <!-- <div id="order-price">$19.99</div> -->
-            </div>
-        </div>
-`;
-
 function AddAllBooks() {
-  let books = JSON.parse(window.localStorage.getItem("books"));
-  const usersArr = JSON.parse(window.localStorage.getItem("users"));
-  let booksID = usersArr[userId].books;
-  console.log(booksID);
-  console.log(booksID.length);
-  if(booksID.length){
-      
+  let request = new XMLHttpRequest();
+  request.open("GET", "http://127.0.0.1:8000/api.BorrowTransaction/");
+  request.send();
+  request.onload = () =>{
+    
   }
-  else{
-      showMessage("this there wrong in AddAllBooks", "red", false)
-      return;
-  }
+
 
   for (let i = 0; i < booksID.length; i += 1) {
       let curBookID = booksID[i];
